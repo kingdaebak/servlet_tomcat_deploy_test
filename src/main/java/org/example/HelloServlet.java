9d166f648fc1cc49d6f4b447d4a7e0f400ac0931
kukiman <kingdaebak@gmail.com>
2020-06-24 23:21:42 +0900

    test commit

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -11,7 +11,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
         resp.setContentType("text/html;charset=UTF-8");
         PrintWriter out = resp.getWriter();
         out.println("Hello World");
